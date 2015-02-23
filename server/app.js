f19bc073fe3ec9af0b965f8f9345c9fb453ead81
balazs4 <balazs4web@gmail.com>
2015-02-23 13:46:37 +0100

    + Replace gulp-notify with node-notifier + Add date-stlylish module

--- server/app.js
+++ server/app.js
@@ -1,63 +1,55 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var gulp = require('gulp');
-var notify = require("gulp-notify");
+var notifier = require('node-notifier');
 var fs = require('fs');
 var http = require('http');
 var path = require('path');
 var os = require('os');
+var datetime = require('date-stylish');
 
 
 var app = express();
 
-app.use(bodyParser.urlencoded(
-{
+app.use(bodyParser.urlencoded({
     extended: true
 }));
 
 
-function base64ToImage(raw, callback)
-{
+function base64ToImage(raw, callback) {
     var coverPath = path.join(os.tmpdir(), "cover.jpg");
     var base64 = raw.replace(/^data:image\/jpeg;base64,/, "");
-    fs.writeFile(coverPath, base64, 'base64', function(err)
-    {
+    fs.writeFile(coverPath, base64, 'base64', function(err) {
         callback(coverPath);
     });
 }
 
 
-
-
 var prevoiousSongId = 0;
 
 app.route('/grooveshark')
-    .post(function(req, res)
-    {
+    .post(function(req, res) {
         var song = req.body;
 
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
         res.status(200).end();
 
-        if (!song || !song.songID || prevoiousSongId == song.songID)
-        {
+        if (!song || !song.songID || prevoiousSongId == song.songID) {
             return;
         }
 
-        console.log("Song changed...");
+
+
+        console.log("[%s] '%s' by '%s' from '%s'", datetime.now, song.songName, song.artistName, song.albumName);
         prevoiousSongId = song.songID;
 
 
-        base64ToImage(song.cover, function(coverFile)
-        {
-            gulp.src(coverFile)
-                .pipe(notify(
-                {
-                    "title": song.songName,
-                    "message": "by '" + song.artistName + "'\nfrom '" + song.albumName + "'",
-                    "icon": coverFile
-                }));
+        base64ToImage(song.cover, function(coverFile) {
+            notifier.notify({
+                "title": song.songName,
+                "message": "by '" + song.artistName + "'\nfrom '" + song.albumName + "'",
+                "icon": coverFile
+            });
         });
     });
 
